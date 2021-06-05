945828b165da92b9d70adbbd66b2ec951c097475
Azhi99 <azhijabar.aj@gmail.com>
2021-06-05 23:01:36 +0300

    commit

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ require('dotenv').config();
 
 const app = express();
 
+const userRouter = require('./routes/user');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.user('/user', userRouter);
 
 app.listen(process.env.PORT, () => {
     console.log("Server Started at port" + process.env.PORT);

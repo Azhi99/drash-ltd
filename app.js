b6d0a6df8528d84d82685455193d4adc22f1445a
Azhi99 <azhijabar.aj@gmail.com>
2021-06-06 18:40:04 +0300

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const app = express();
 
-// const userRouter = require('./routes/user');
+const userRouter = require('./routes/user');
 const emailRouter = require('./routes/emails.js')
 const contactRouter = require('./routes/contactUs')
 const aboutRouter = require('./routes/about.js')
@@ -11,7 +11,7 @@ const aboutRouter = require('./routes/about.js')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.user('/user', userRouter);
+app.use('/users', userRouter);
 app.use('/emails', emailRouter)
 app.use('/contactUs', contactRouter)
 app.use('/aboutUs', aboutRouter)
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,83 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const db = require('../db/dbConfig');
 const router = express.Router();
 
-router.post('/addUser', (req, res) => {
-    
+router.post('/addUser', async (req, res) => {
+    try {
+        const [u_id] = await db('tbl_users').insert({
+            username: req.body.username,
+            password: bcrypt.hashSync(req.body.password, 12),
+            status: '1'
+        });
+        return res.status(200).send({ u_id });
+    } catch (error) {
+        if(error.errno == 1062) {
+            return res.status(500).send({
+                message: 'This username already exist'
+            });
+        }
+    }
 });
+
+router.patch('/updateUser/:u_id', async (req, res) => {
+    try {
+        await db('tbl_users').where('u_id', req.params.u_id).update({
+            username: req.body.username
+        });
+        return res.sendStatus(200);
+    } catch (error) {
+        if(error.errno == 1062) {
+            return res.status(500).send({
+                message: 'This username already exist'
+            });
+        }
+    }
+});
+
+router.patch('/updatePassword/:u_id', async (req, res) => {
+    try {
+        await db('tbl_users').where('u_id', req.params.u_id).update({
+            password: bcrypt.hashSync(req.body.password, 12)
+        });
+        return res.sendStatus(200);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+});
+
+router.patch('/deactiveUser/:u_id', async (req, res) => {
+    const [{noOfActived}] = await db('tbl_users').where('status', '1').count('* as noOfActived');
+    const [{userStatus}] = await db('tbl_users').where('u_id', req.params.u_id).select(['status as userStatus']);
+    if(noOfActived > 1 && userStatus == 1) {
+        await db('tbl_users').where('u_id', req.params.u_id).update({
+            status: '0'
+        });
+        return res.sendStatus(200);
+    }
+    return res.status(500).send({
+        message: 'You can not deactive this user'
+    });
+});
+
+router.patch('/activeUser/:u_id', async (req, res) => {
+    try {
+        await db('tbl_users').where('u_id', req.params.u_id).update({
+            status: '1'
+        });
+        return res.sendStatus(200);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+});
+
+router.delete('/deleteUser/:u_id', async (req, res) => {
+    try {
+        await db('tbl_users').where('u_id', req.params.u_id).delete();
+        return res.sendStatus(200);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+})
 
 module.exports = router;

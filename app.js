43e44d6f8aa016b18b71951e9a244a4b75d6b7af
Azhi99 <azhijabar.aj@gmail.com>
2021-06-07 20:14:44 +0300

    commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 const userRouter = require('./routes/user');
 const certificateRouter = require('./routes/certificate');
+const serviceRouter = require('./routes/service');
 const emailRouter = require('./routes/emails.js')
 const contactRouter = require('./routes/contactUs')
 const aboutRouter = require('./routes/about.js')
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/users', userRouter);
 app.use('/certificates', certificateRouter);
+app.use('/services', serviceRouter);
 app.use('/emails', emailRouter)
 app.use('/contactUs', contactRouter)
 app.use('/aboutUs', aboutRouter)
---
+++ routes/service.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const db = require('../db/dbConfig');
+const router = express.Router();
+
+const fileStorage = multer.diskStorage({
+    destination: './public/serviceImages',
+    filename: (req, file, cb) => {
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: fileStorage,
+    fileFilter: (req, file, cb) => {
+        if(['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimetype)){
+            cb(null, true);
+        } else {
+            cb(null, false);
+            cb(new Error('Invalid file type'));
+        }
+    }
+}).single('serviceImage');
+
+router.post('/addService', async (req, res) => {
+    upload(req, res, async (err) => {
+        if(!err){
+            try {
+                await db('tbl_services').insert({
+                    title_en: req.body.title_en,
+                    title_ku: req.body.title_ku,
+                    title_ar: req.body.title_ar,
+                    desc_en: req.body.desc_en,
+                    desc_ku: req.body.desc_ku,
+                    desc_ar: req.body.desc_ar,
+                    image_path: './serviceImages/' + req.file.filename
+                });
+                return res.sendStatus(200);
+            } catch (error) {
+                return res.status(500).send({
+                    error
+                })
+            }
+        } else {
+            return res.status(500).send({
+                message: 'File type may invalid'
+            })
+        }
+    });
+});
+
+router.patch('/updateImage/:s_id', (req, res) => {
+    upload(req, res, async (err) => {
+        if(!err) {
+            try {
+                const [{oldImage}] = await db('tbl_services').where('s_id', req.params.s_id).select(['image_path as oldImage']);
+                await db('tbl_services').where('s_id', req.params.s_id).update({
+                    image_path: './serviceImages/' + req.file.filename
+                });
+                fs.unlinkSync('./public' + oldImage.slice(1));
+                return res.sendStatus(200);
+            } catch (error) {
+                return res.status(500).send({ error });
+            }
+        }
+        return res.status(500).send({
+            message: 'File type may invalid'
+        });
+    });
+});
+
+module.exports = router;
